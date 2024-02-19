22db9f6ee6abb73c7fc909ad67898a492376c727
Adriano Bruzzese <146066663+adrianobruzzese@users.noreply.github.com>
2024-02-19 11:07:56 +0100

    Update store.js

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,11 +1,1 @@
-import { configureStore } from '@reduxjs/toolkit';
-import rootReducer from './rootReducer'; 
 
-
-const store = configureStore({
-  reducer: rootReducer,
-
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(/* altri middleware qui */),
-});
-
-export default store;
