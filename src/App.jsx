cd20d8cb6b7f1768c40b32e20110d857e1e7e958
Fabio Gilardi <fabiogilardi02@gmail.com>
2024-02-19 11:53:21 +0100

    app basic structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import "./style/style.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
 
 function App() {
   return (
     <>
-      <h1 className="text-primary">
+      {/* <h1 className="text-primary">
         Ciao <i className="bi bi-0-circle-fill"></i>
-      </h1>
+      </h1> */}
+      <Container>
+        <Row>
+          {/* MAIN */}
+          {/* SIDEBAR */}
+        </Row>
+      </Container>
     </>
   );
 }
