3f47bb5b4a534c62df16f81c0c60f015c0c8b913
Adriano Bruzzese <146066663+adrianobruzzese@users.noreply.github.com>
2024-02-20 15:43:34 +0100

    deleted common folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,28 @@
-// import './style/style.css';
-// import 'bootstrap-icons/font/bootstrap-icons.css';
-// import Body from './components/Body';
-// import Consigliato from './components/Consigliato';
-// import Analisi from './components/Analisi';
-// import Risorse from './components/Risorse';
-// import Attivita from './components/Attivita';
-// import Esperienza from './components/Esperienza';
-// import Innteressi from './components/Interessi';
-// import CustomNavbar from './components/CustomNavbar'
-// function App() {
-//   return (
-//     <>
-//       <CustomNavbar/>
-//       <Body />
-//       <Consigliato />
-//       <Analisi />
-//       <Risorse />
-//       <Attivita />
-//       <Esperienza />
-//       <Innteressi />
-//     </>
-//   );
-// }
+ import './style/style.css';
+ import 'bootstrap-icons/font/bootstrap-icons.css';
+ import Body from './components/Body';
+ import Consigliato from './components/Consigliato';
+ import Analisi from './components/Analisi';
+ import Risorse from './components/Risorse';
+ import Attivita from './components/Attivita';
+ import Esperienza from './components/Esperienza';
+ import Innteressi from './components/Interessi';
+ import CustomNavbar from './components/CustomNavbar'
+ function App() {
+   return (
+     <>
+       <CustomNavbar/>
+       <Body />
+       <Consigliato />
+       <Analisi />
+       <Risorse />
+       <Attivita />
+       <Esperienza />
+       <Innteressi />
+     </>
+   );
+ }
 
-// export default App;
+ export default App;
 
-import './style/style.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap-icons/font/bootstrap-icons.css';
-import RightSidebar from './components/home/RightSidebar';
 
-function App() {
-  return (
-    <>
-      <RightSidebar />
-    </>
-  );
-}
-
-export default App;
