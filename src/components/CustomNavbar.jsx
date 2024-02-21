f989fb51f8fc3ec663eb241e790c164c8cfbcff2
Salvatore-Malanchino <malanchinos@hotmail.it>
2024-02-21 16:57:25 +0100

    .

--- src/components/CustomNavbar.jsx
+++ src/components/CustomNavbar.jsx
@@ -19,144 +19,170 @@ function NavbarComponent() {
         sticky="top"
         style={{ height: '55px' }}
       >
-        <Container fluid>
-          <Navbar.Brand href="#Logo1">
-            <Image
-              src={Logo}
-              S
-              width="40px"
-              className="d-inline-block align-top ms-5"
-              alt="Logo"
-              loading="lazy"
-            />
-          </Navbar.Brand>
-          <Nav.Link href="#search" className="me-2">
-            <i className="bi bi-search"></i>
-          </Nav.Link>
-          <Form inline>
-            <Row>
-              <Col xs="auto">
-                <Form.Control
-                  type="text"
-                  placeholder="Search"
-                  className=" mr-sm-2"
+        <Container fluid className=" justify-content-evenly ">
+          <div className="d-flex">
+            <div className="d-flex align-items-center border-end">
+              <Navbar.Brand href="#Logo1">
+                <Image
+                  src={Logo}
+                  S
+                  width="40px"
+                  className="d-inline-block align-top ms-5"
+                  alt="Logo"
+                  loading="lazy"
                 />
-              </Col>
-            </Row>
-          </Form>
-          <Navbar.Collapse className="mobile-collapse">
-            <Nav className="ms-5">
-              <Nav.Link href="#home" className="me-1 mt-2">
-                <i
-                  className="bi bi-house-door-fill"
-                  style={{
-                    fontSize: '20px',
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                    gap: '15px',
-                  }}
-                ></i>
-                Home
+              </Navbar.Brand>
+              <Nav.Link href="#search" className="me-2">
+                <i className="bi bi-search"></i>
               </Nav.Link>
-              <Nav.Link href="#" className="me-1 mt-2">
-                {' '}
-                <i
-                  className="bi bi-people-fill"
-                  style={{
-                    fontSize: '20px',
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                    gap: '15px',
-                  }}
-                ></i>
-                My Network
-              </Nav.Link>
-              <Nav.Link href="#" className="me-1 mt-2">
-                <i
-                  className="bi bi-briefcase-fill"
-                  style={{
-                    fontSize: '20px',
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                    gap: '15px',
-                  }}
-                ></i>
-                Jobs
-              </Nav.Link>
-              <Nav.Link href="#" className="me-1 mt-2">
-                <i
-                  className="bi bi-chat-dots-fill"
-                  style={{
-                    fontSize: '20px',
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                    gap: '15px',
-                  }}
-                ></i>
-                Messaging
-              </Nav.Link>
-              <Nav.Link href="#" className="mt-2 me-4">
-                <i
-                  className="bi bi-bell-fill"
-                  style={{
-                    fontSize: '20px',
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                    gap: '15px',
-                  }}
-                ></i>
-                Notifications
-              </Nav.Link>
-              <Nav style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
-  <img
-    src="https://picsum.photos/200"
-    className="rounded-circle border border-secondary mt-2"
-    style={{ width: '25px', height: '25px' }} 
-    alt="profile picture"
-  />
-  <NavDropdown
-    title="Me"
-    align={{ lg: 'end', md: 'end', sm: 'end', xs: 'end' }}
-    style={{ fontSize:'1rem', marginBottom:'3px'}} 
-  >
-    <NavDropdown.Item href="#gestioni-profili">
-      Gestione Profili
-    </NavDropdown.Item>
-    <NavDropdown.Item href="#account">Account</NavDropdown.Item>
-    <NavDropdown.Divider />
-    <NavDropdown.Item href="#logout">Logout</NavDropdown.Item>
-  </NavDropdown>
-</Nav>
+              <Form inline>
+                <Row className="me-5">
+                  <Col xs="auto">
+                    <Form.Control
+                      type="text"
+                      placeholder="Search"
+                      className=" mr-sm-2"
+                    />
+                  </Col>
+                </Row>
+              </Form>
 
-            </Nav>
-          </Navbar.Collapse>
-          <Navbar.Collapse>
-  <Nav style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-    <i
-      className="bi bi-grid-3x3-gap-fill mt-4"
-      style={{
-        fontSize: '20px' 
-      }}
-    ></i>
-    <NavDropdown
-      title="For business" className="mb-3"
-      align={{ lg: 'end', md: 'end', sm: 'end', xs: 'end' }}
-    >
-      <NavDropdown.Item href="#gestioni-profili">
-        Acquire premium membership
-      </NavDropdown.Item>
-      <NavDropdown.Item href="#account">Account</NavDropdown.Item>
-      <NavDropdown.Divider />
-      <NavDropdown.Item href="#logout">Logout</NavDropdown.Item>
-    </NavDropdown>
-  </Nav>
-</Navbar.Collapse>
+              <Navbar.Collapse className="mobile-collapse justify-content-end ">
+                <Nav className="ms-5">
+                  <Nav.Link href="#home" className="me-1 mt-2">
+                    <i
+                      className="bi bi-house-door-fill"
+                      style={{
+                        fontSize: '20px',
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                        gap: '15px',
+                      }}
+                    ></i>
+                    Home
+                  </Nav.Link>
+                  <Nav.Link href="#" className="me-1 mt-2">
+                    {' '}
+                    <i
+                      className="bi bi-people-fill"
+                      style={{
+                        fontSize: '20px',
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                        gap: '15px',
+                      }}
+                    ></i>
+                    My Network
+                  </Nav.Link>
+                  <Nav.Link href="#" className="me-1 mt-2">
+                    <i
+                      className="bi bi-briefcase-fill"
+                      style={{
+                        fontSize: '20px',
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                        gap: '15px',
+                      }}
+                    ></i>
+                    Jobs
+                  </Nav.Link>
+                  <Nav.Link href="#" className="me-1 mt-2">
+                    <i
+                      className="bi bi-chat-dots-fill"
+                      style={{
+                        fontSize: '20px',
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                        gap: '15px',
+                      }}
+                    ></i>
+                    Messaging
+                  </Nav.Link>
+                  <Nav.Link href="#" className="mt-2 me-4">
+                    <i
+                      className="bi bi-bell-fill"
+                      style={{
+                        fontSize: '20px',
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                        gap: '15px',
+                      }}
+                    ></i>
+                    Notifications
+                  </Nav.Link>
+                  <Nav
+                    className="me-2"
+                    style={{
+                      display: 'flex',
+                      flexDirection: 'column',
+                      alignItems: 'center',
+                      justifyContent: 'center',
+                    }}
+                  >
+                    <img
+                      src="https://picsum.photos/200"
+                      className="rounded-circle border border-secondary mt-2"
+                      style={{ width: '25px', height: '25px' }}
+                      alt="profile picture"
+                    />
 
+                    <NavDropdown
+                      title="Me"
+                      align={{ lg: 'end', md: 'end', sm: 'end', xs: 'end' }}
+                      style={{
+                        fontSize: '1rem',
+                        marginBottom: '3px',
+                        padding: '0',
+                      }}
+                      className="p-0"
+                    >
+                      <NavDropdown.Item href="#gestioni-profili">
+                        Gestione Profili
+                      </NavDropdown.Item>
+                      <NavDropdown.Item href="#account">
+                        Account
+                      </NavDropdown.Item>
+                      <NavDropdown.Divider />
+                      <NavDropdown.Item href="#logout">Logout</NavDropdown.Item>
+                    </NavDropdown>
+                  </Nav>
+                </Nav>
+              </Navbar.Collapse>
+            </div>
+            <Navbar.Collapse>
+              <Nav
+                style={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  alignItems: 'center',
+                }}
+              >
+                <i
+                  className="bi bi-grid-3x3-gap-fill mt-4"
+                  style={{
+                    fontSize: '20px',
+                  }}
+                ></i>
+                <NavDropdown
+                  title="For business"
+                  className="mb-3"
+                  align={{ lg: 'end', md: 'end', sm: 'end', xs: 'end' }}
+                >
+                  <NavDropdown.Item href="#gestioni-profili">
+                    Acquire premium membership
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="#account">Account</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#logout">Logout</NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+            </Navbar.Collapse>
+          </div>
         </Container>
       </Navbar>
     </>
