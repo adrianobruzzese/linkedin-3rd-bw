368f3f099a435fe8c15aa5a073611781fe1a9adf
Marco Brunetti <151136910+marcocodd@users.noreply.github.com>
2024-02-21 09:56:07 +0100

    nessuna modifica particolare

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 import './style/style.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import CustomNavbar from './components/CustomNavbar';
-import Profile from './components/Profile';
-import Jobs from './components/Jobs';
-import HomePage from "./components/HomePage";
+ import Profile from "./components/Profile";
+ import Jobs from "./components/Jobs";
+ import HomePage from "./components/HomePage";
 
-function App() {
- return (
-  <>
-   <CustomNavbar />
-   {/* <Profile />
-   <Jobs /> */}
-   {/* <HomePage /> */}
-  </>
- );
-}
+ function App() {
+  return (
+   <>
+    <CustomNavbar />
+    {/* <Jobs />  */}
+    {/* <HomePage /> */}
 
+    {/* <Profile /> */}
+   </>
+  );
+ }
 export default App;
