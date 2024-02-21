15a4e6d5706829bc91a095b3bfc360483506dc5c
Salvatore-Malanchino <malanchinos@hotmail.it>
2024-02-21 10:16:16 +0100

    import

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import './style/style.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-import CustomNavbar from './components/CustomNavbar';
-import Profile from './components/Profile';
 import Jobs from './components/Jobs';
-
+import HomePage from './components/HomePage';
 function App() {
   return (
     <>
       {/* <CustomNavbar />
       <Profile /> */}
-      <Jobs />
+      {/* <Jobs /> */}
+      <HomePage />
     </>
   );
 }
