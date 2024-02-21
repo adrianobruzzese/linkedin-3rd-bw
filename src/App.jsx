36d623e1212d98a76fc32674edb9037a5c96e361
Adriano Bruzzese <146066663+adrianobruzzese@users.noreply.github.com>
2024-02-21 23:57:28 +0100

    late base commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import './style/style.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import CustomNavbar from './components/CustomNavbar';
-//  import Profile from "./components/Profile";
-//  import Jobs from "./components/Jobs";
+// import Profile from "./components/Profile";
+// import Jobs from './components/Jobs';
 import HomePage from './components/HomePage';
 
 function App() {
   return (
     <>
       <CustomNavbar />
-      <HomePage />
+      <HomePage /> 
       {/* <Profile /> */}
 
       {/* <Jobs /> */}
