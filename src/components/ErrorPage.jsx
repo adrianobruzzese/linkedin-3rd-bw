ebd4ec0aa24e0c7c1abeacc3dbd29db4027896c9
Fabio Gilardi <fabiogilardi02@gmail.com>
2024-02-22 09:54:35 +0100

    routes added

---
+++ src/components/ErrorPage.jsx
@@ -0,0 +1,13 @@
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+const ErrorPage = () => (
+  <Container>
+    <Row>
+      <Col sm={10} md={8} lg={6}></Col>
+    </Row>
+  </Container>
+);
+
+export default ErrorPage;
