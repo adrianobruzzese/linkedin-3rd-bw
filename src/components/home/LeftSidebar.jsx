09f28449b65f599983a221e40b37ceb87224e283
Salvatore-Malanchino <malanchinos@hotmail.it>
2024-02-22 14:52:57 +0100

    commit

--- src/components/home/LeftSidebar.jsx
+++ src/components/home/LeftSidebar.jsx
@@ -2,39 +2,40 @@
 // import 'bootstrap-icons/font/bootstrap-icons.css';
 // import '../../style/style.css';
 
-import { Link } from "react-router-dom";
+import { Row, Col } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const LeftSidebar = () => {
   return (
     <div>
-      <div className="card profile-card mx-auto" style={{ maxWidth: "280px" }}>
+      <div className="card profile-card mx-auto" style={{ maxWidth: '280px' }}>
         <img
           src="https://picsum.photos/200"
           className="card-img-top rounded-circle border border-secondary mb-4 mt-4 mx-auto"
-          style={{ width: "100px" }}
+          style={{ width: '100px' }}
           alt="profile picture"
         />
         <div className="card-body text-center">
-          <Link to={"/profile"} className="nav-link">
+          <Link to={'/profile'} className="nav-link">
             <h5 className="card-title">Adriano Bruzzese</h5>
           </Link>
           <p className="card-text text-muted mb-3">Bar Manager</p>
           <div
             className="d-flex justify-content-between mt-3"
-            style={{ borderTop: "0.7px solid grey" }}
+            style={{ borderTop: '0.7px solid grey' }}
           >
             <p className="text-muted mb-0 mt-3">Connections</p>
             <p className="font-weight-bold text-primary mb-0 mt-3">20</p>
           </div>
           <div
             className="d-flex font-weight-bold"
-            style={{ borderBottom: "0.7px solid grey" }}
+            style={{ borderBottom: '0.7px solid grey' }}
           >
             <p className="align-content-start">Grow your Network</p>
           </div>
           <div
             className="text-start mt-2"
-            style={{ borderBottom: "0.7px solid grey" }}
+            style={{ borderBottom: '0.7px solid grey' }}
           >
             <p className="text-muted">
               Strengthen your profile with an AI writing assistant
@@ -49,16 +50,17 @@ const LeftSidebar = () => {
           </div>
         </div>
       </div>
+
       <div
-        className="card profile-card mx-auto mt-3"
-        style={{ maxWidth: "280px" }}
+        className="card profile-card mx-auto mt-3 d-sm-none d-md-block "
+        style={{ maxWidth: '280px' }}
       >
         <div>
           <p className="text-start text-muted mt-2 ms-3 mb-0">Recent</p>
           <p className="text-start text-muted mt-2 ms-3 mb-0">
             <span
               className="me-2"
-              style={{ fontSize: "1.5rem", fontWeight: "bold" }}
+              style={{ fontSize: '1.5rem', fontWeight: 'bold' }}
             >
               #
             </span>
@@ -67,7 +69,7 @@ const LeftSidebar = () => {
           <p className="text-start text-muted ms-3">
             <span
               className="me-2"
-              style={{ fontSize: "1.5rem", fontWeight: "bold" }}
+              style={{ fontSize: '1.5rem', fontWeight: 'bold' }}
             >
               #
             </span>
@@ -82,7 +84,7 @@ const LeftSidebar = () => {
           </div>
           <div className="d-flex justify-content-between">
             <p className="text-start text-primary fw-bold mt-3 ms-3">Events</p>
-            <span className="me-2" style={{ fontSize: "2rem" }}>
+            <span className="me-2" style={{ fontSize: '2rem' }}>
               +
             </span>
           </div>
@@ -93,7 +95,7 @@ const LeftSidebar = () => {
             <p className="text-start text-muted mt-2 ms-3 mb-0">
               <span
                 className="me-2"
-                style={{ fontSize: "1.5rem", fontWeight: "bold" }}
+                style={{ fontSize: '1.5rem', fontWeight: 'bold' }}
               >
                 #
               </span>
@@ -102,7 +104,7 @@ const LeftSidebar = () => {
             <p className="text-start text-muted ms-3">
               <span
                 className="me-2"
-                style={{ fontSize: "1.5rem", fontWeight: "bold" }}
+                style={{ fontSize: '1.5rem', fontWeight: 'bold' }}
               >
                 #
               </span>
@@ -113,7 +115,7 @@ const LeftSidebar = () => {
         </div>
         <div
           className="text-center mt-2"
-          style={{ borderTop: "0.7px solid grey" }}
+          style={{ borderTop: '0.7px solid grey' }}
         >
           <p className="text-muted mt-3 fw-bold">Discover more</p>
         </div>
