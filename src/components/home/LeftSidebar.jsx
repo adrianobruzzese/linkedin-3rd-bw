b4dad5b542639a29de000ebedcd2efd4a6ad00c9
Adriano Bruzzese <146066663+adrianobruzzese@users.noreply.github.com>
2024-02-22 01:29:53 +0100

    additional left card added

--- src/components/home/LeftSidebar.jsx
+++ src/components/home/LeftSidebar.jsx
@@ -116,7 +116,6 @@ const LeftSidebar = () => {
     <Container id="sticky-sidebars">
       <Row className="justify-content-center">
         <Col xs={12}>
-          {/* Profile Card */}
           <Card className="mb-3">
             <Card.Img
               variant="top"
@@ -146,20 +145,88 @@ const LeftSidebar = () => {
                 >
                   Strengthen your profile with an AI writing assistant
                 </ListGroup.Item>
-                <ListGroup.Item className='pb-2'>
-                  <i className="bi bi-square-fill" style={{color: 'orange'}}></i> Retry Premium for €0
+                <ListGroup.Item className="pb-2">
+                  <i
+                    className="bi bi-square-fill"
+                    style={{ color: 'orange' }}
+                  ></i>{' '}
+                  Retry Premium for €0
                 </ListGroup.Item>
-                <ListGroup.Item className='mt-2'>
-              <i className="bi bi-bookmark-fill"></i> My Items
-            </ListGroup.Item>
+                <ListGroup.Item className="mt-2">
+                  <i className="bi bi-bookmark-fill"></i> My Items
+                </ListGroup.Item>
               </ListGroup>
-              
             </Card.Body>
-            
-            
           </Card>
 
-         
+          <Card
+            className="profile-card mx-auto mt-3"
+            style={{ maxWidth: '280px' }}
+          >
+            <ListGroup variant="flush">
+              <ListGroup.Item className="text-start text-muted">
+                <p className="mt-3 ms-3 mb-0">Recent</p>
+                <p className="mt-2 ms-3 mb-0">
+                  <span
+                    className="me-1"
+                    style={{ fontSize: '1.1rem', fontWeight: 'bold' }}
+                  >
+                    #
+                  </span>
+                  hospitalitycareers
+                </p>
+                <p className="ms-3">
+                  <span
+                    className="me-2"
+                    style={{ fontSize: '1.1rem', fontWeight: 'bold' }}
+                  >
+                    #
+                  </span>
+                  hospitalityindustry
+                </p>
+              </ListGroup.Item>
+              <ListGroup.Item className="text-start">
+                <p className="text-primary fw-bold ms-3 mb-0">Groups</p>
+              </ListGroup.Item>
+              <ListGroup.Item className="d-flex justify-content-between align-items-center text-start">
+                <p className="text-primary fw-bold ms-3 mb-0">Events</p>
+                <span className="me-2 " style={{ fontSize: '1.5rem' }}>
+                  +
+                </span>
+              </ListGroup.Item>
+
+              <ListGroup.Item>
+                <p className="text-primary fw-bold ms-3 mb-0">
+                  Followed Hashtags
+                </p>
+                <p className="text-muted mt-2 ms-3 mb-0">
+                  <span
+                    className="me-2"
+                    style={{ fontSize: '1.1rem', fontWeight: 'bold' }}
+                  >
+                    #
+                  </span>
+                  hospitalitycareers
+                </p>
+                <p className="text-start text-muted ms-3">
+                  <span
+                    className="me-2"
+                    style={{ fontSize: '1.1rem', fontWeight: 'bold' }}
+                  >
+                    #
+                  </span>
+                  hospitalityindustry
+                </p>
+                <p className="text-start text-muted ms-3">See all</p>
+              </ListGroup.Item>
+            </ListGroup>
+            <Card.Footer
+              className="text-center"
+              style={{ borderTop: '0.7px solid grey' }}
+            >
+              <p className="text-muted mt-1 mb-1 fw-bold">Discover more</p>
+            </Card.Footer>
+          </Card>
         </Col>
       </Row>
     </Container>
