bf76406dd414b53442a8d46b72f3c7f88c09dc09
Adriano Bruzzese <146066663+adrianobruzzese@users.noreply.github.com>
2024-02-23 12:26:29 +0100

    commit before merge 12.26pm

--- src/components/HomePagePosts.jsx
+++ src/components/HomePagePosts.jsx
@@ -3,70 +3,69 @@ import { useDispatch, useSelector } from 'react-redux';
 import { FetchDataPosts } from '../redux/actions/FetchDataPostsAction';
 import { Button, Card, Col } from 'react-bootstrap';
 import defaultUserImg from '../assets/img/default-profile-picture1.jpg';
-import { Modal } from "react-bootstrap";
-import { Form } from "react-bootstrap";
+import { Modal } from 'react-bootstrap';
+import { Form } from 'react-bootstrap';
 
 const HomePagePosts = () => {
- const dispatch = useDispatch();
- const arrayPosts = useSelector((state) => state.posts.posts); // slice per ottenere solo i primi 20 post
- const [arrayPostsSliced, setArrayPostsSliced] = useState([]);
- // LIKE
- const [liked, setLiked] = useState(false);
+  const dispatch = useDispatch();
+  const arrayPosts = useSelector((state) => state.posts.posts); // slice per ottenere solo i primi 20 post
+  const [arrayPostsSliced, setArrayPostsSliced] = useState([]);
+  // LIKE
+  const [liked, setLiked] = useState(false);
 
- //stato modale
- const [smShow, setSmShow] = useState(false);
+  //stato modale
+  const [smShow, setSmShow] = useState(false);
 
- const handleClick = () => {
-  setLiked(!liked);
- };
- //   COMMENTI
- // const [isCommenting, setIsCommenting] = useState(false);
- const [commentingStates, setCommentingStates] = useState([]);
+  const handleClick = () => {
+    setLiked(!liked);
+  };
+  //   COMMENTI
+  // const [isCommenting, setIsCommenting] = useState(false);
+  const [commentingStates, setCommentingStates] = useState([]);
 
- useEffect(() => {
-  dispatch(FetchDataPosts());
- }, []);
+  useEffect(() => {
+    dispatch(FetchDataPosts());
+  }, []);
 
- useEffect(() => {
-  if (arrayPosts.length > 0) {
-   setArrayPostsSliced(arrayPosts.slice(-10));
-   setCommentingStates(arrayPosts.slice(-10).map(() => false));
+  useEffect(() => {
+    if (arrayPosts.length > 0) {
+      setArrayPostsSliced(arrayPosts.slice(-10));
+      setCommentingStates(arrayPosts.slice(-10).map(() => false));
 
-  const generateRandomFollowers = () =>
-    Math.floor(Math.random() * (99999 - 1000 + 1)) + 1000;
-  const generateRandomDay = () => Math.floor(Math.random() * 4) + 1;
+      console.log('post array slice', arrayPostsSliced);
+    }
+  }, [arrayPosts]);
+
+  const toggleCommentSection = (i) => {
+    const newCommentingStates = [...commentingStates];
+    newCommentingStates[i] = !newCommentingStates[i];
+    setCommentingStates(newCommentingStates);
+  };
 
   return (
     <>
       {arrayPosts.slice(-10).map((post, i) => (
-        <Col className="col-12 p-0 mt-1 mb-2" key={i}>
+        <Col className="col-12 p-0" key={i}>
           <Card className="p-2">
-            <Card.Body className="border-bottom">
-              <div className="d-flex align-items-start">
+            <Card.Body className="border-bottom mb-2">
+              <div className="d-flex align-items-center">
                 {post.user.image ? (
                   <img
                     className="rounded-circle me-2"
                     src={post.user.image}
-                    width={50}
+                    width={55}
+                    height={55}
                     alt="User profile"
                   />
                 ) : (
                   <img
                     className="rounded-circle me-2"
                     src={defaultUserImg}
-                    width={50}
+                    width={55}
                     alt="Default profile"
                   />
                 )}
-<div className='ms-1'>
-                <Card.Title className="fs-6 mb-1">{post.username}</Card.Title>
-                <div className="text-muted" style={{fontSize: '0.7rem'}}>
-                    Followers: {generateRandomFollowers()}
-                  </div>
-                  <div className="text-muted" style={{fontSize: '0.7rem'}}>
-                  {generateRandomDay()}d ago
-                  </div>
-                  </div>
+                <Card.Title className="me-2 fs-6">{post.username}</Card.Title>
               </div>
               <span>{post.user.title}</span>
               <Card.Text>{post.text}</Card.Text>
@@ -81,7 +80,7 @@ const HomePagePosts = () => {
               )}
               {/* //    <img className="" width={250} alt="img post" src={post.image} /> */}
             </Card.Body>
-            <div className="d-flex justify-content-evenly mt-1">
+            <div className="d-flex justify-content-evenly">
               <Button
                 className={`d-flex align-items-center button-homepage${
                   liked ? ' text-primary' : ''
@@ -97,21 +96,24 @@ const HomePagePosts = () => {
               </Button>
               <Button
                 className="d-flex align-items-center button-homepage"
-                onClick={toggleCommentSection}
+                onClick={() => {
+                  toggleCommentSection(i);
+                  setSmShow(true);
+                }}
               >
                 <i className="bi bi-chat-left-dots me-2"></i>
                 <span>Comment</span>
               </Button>
-              {isCommenting && (
-                <div className="comment-section d-flex align-items-center">
-                  <textarea placeholder="Inserisci il tuo commento"></textarea>
-                  <div>
-                    <Button className="ms-1" variant="outline-info">
-                      Invia
-                    </Button>
-                  </div>
-                </div>
-              )}
+              {/* {commentingStates[i] && (
+        // <div className="comment-section d-flex align-items-center">
+        //  <textarea placeholder="Inserisci il tuo commento"></textarea>
+        //  <div>
+        // <Button className="ms-1" variant="outline-info">
+        //  Invia
+        // </Button>
+        //  </div>
+        // </div>
+       )} */}
               <Button className="button-homepage">
                 <i className="bi bi-repeat me-2"></i>Repost
               </Button>
@@ -122,15 +124,35 @@ const HomePagePosts = () => {
           </Card>
         </Col>
       ))}
+      {/* MODALE COMMENTI */}
+
+      <Modal
+        size="sm"
+        show={smShow}
+        onHide={() => setSmShow(false)}
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">Comment</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group
+              className="mb-3"
+              controlId="exampleForm.ControlTextarea1"
+            >
+              <Form.Control
+                as="textarea"
+                rows={3}
+                placeholder="Write your comment here"
+              />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+      </Modal>
     </>
   );
 };
-
-
-
-
-
-
-
 
 export default HomePagePosts;
