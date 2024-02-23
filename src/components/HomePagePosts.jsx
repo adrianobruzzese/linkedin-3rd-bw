062a0bb74b3c7d2c66989e229b5a95898933b22e
Adriano Bruzzese <146066663+adrianobruzzese@users.noreply.github.com>
2024-02-23 11:49:17 +0100

    added followers number and days

--- src/components/HomePagePosts.jsx
+++ src/components/HomePagePosts.jsx
@@ -31,29 +31,41 @@ const HomePagePosts = () => {
     }
   }, [arrayPosts]);
 
+  const generateRandomFollowers = () =>
+    Math.floor(Math.random() * (99999 - 1000 + 1)) + 1000;
+  const generateRandomDay = () => Math.floor(Math.random() * 4) + 1;
+
   return (
     <>
       {arrayPosts.slice(-10).map((post, i) => (
         <Col className="col-12 p-0 mt-1 mb-2" key={i}>
           <Card className="p-2">
             <Card.Body className="border-bottom">
-              <div className="d-flex align-items-center">
+              <div className="d-flex align-items-start">
                 {post.user.image ? (
                   <img
                     className="rounded-circle me-2"
                     src={post.user.image}
-                    width={55}
+                    width={50}
                     alt="User profile"
                   />
                 ) : (
                   <img
                     className="rounded-circle me-2"
                     src={defaultUserImg}
-                    width={55}
+                    width={50}
                     alt="Default profile"
                   />
                 )}
-                <Card.Title className="me-2 fs-6">{post.username}</Card.Title>
+<div className='ms-1'>
+                <Card.Title className="fs-6 mb-1">{post.username}</Card.Title>
+                <div className="text-muted" style={{fontSize: '0.7rem'}}>
+                    Followers: {generateRandomFollowers()}
+                  </div>
+                  <div className="text-muted" style={{fontSize: '0.7rem'}}>
+                  {generateRandomDay()}d ago
+                  </div>
+                  </div>
               </div>
               <span>{post.user.title}</span>
               <Card.Text>{post.text}</Card.Text>
