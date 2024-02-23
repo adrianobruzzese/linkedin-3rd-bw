68fca5814c76f97150b42b17b20fb89f63c595f3
Adriano Bruzzese <146066663+adrianobruzzese@users.noreply.github.com>
2024-02-23 14:52:57 +0100

    fixed posts

--- src/components/HomePagePosts.jsx
+++ src/components/HomePagePosts.jsx
@@ -3,8 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { FetchDataPosts } from '../redux/actions/FetchDataPostsAction';
 import { Button, Card, Col } from 'react-bootstrap';
 import defaultUserImg from '../assets/img/default-profile-picture1.jpg';
-import { Modal } from 'react-bootstrap';
-import { Form } from 'react-bootstrap';
+
 
 const HomePagePosts = () => {
   const dispatch = useDispatch();
@@ -32,6 +31,8 @@ const HomePagePosts = () => {
       setArrayPostsSliced(arrayPosts.slice(-10));
       setCommentingStates(arrayPosts.slice(-10).map(() => false));
 
+      c
+
       console.log('post array slice', arrayPostsSliced);
     }
   }, [arrayPosts]);
@@ -42,30 +43,42 @@ const HomePagePosts = () => {
     setCommentingStates(newCommentingStates);
   };
 
+  const generateRandomFollowers = () =>
+        Math.floor(Math.random() * (99999 - 1000 + 1)) + 1000;
+      const generateRandomDay = () => Math.floor(Math.random() * 4) + 1;
+
+
   return (
     <>
       {arrayPosts.slice(-10).map((post, i) => (
-        <Col className="col-12 p-0" key={i}>
+        <Col className="col-12 p-0 mt-1 mb-2" key={i}>
           <Card className="p-2">
-            <Card.Body className="border-bottom mb-2">
-              <div className="d-flex align-items-center">
+            <Card.Body className="border-bottom">
+              <div className="d-flex align-items-start">
                 {post.user.image ? (
                   <img
                     className="rounded-circle me-2"
                     src={post.user.image}
-                    width={55}
-                    height={55}
+                    width={50}
                     alt="User profile"
                   />
                 ) : (
                   <img
                     className="rounded-circle me-2"
                     src={defaultUserImg}
-                    width={55}
+                    width={50}
                     alt="Default profile"
                   />
                 )}
-                <Card.Title className="me-2 fs-6">{post.username}</Card.Title>
+                <div className="ms-1">
+                  <Card.Title className="fs-6 mb-1">{post.username}</Card.Title>
+                  <div className="text-muted" style={{ fontSize: '0.7rem' }}>
+                    Followers: {generateRandomFollowers()}
+                  </div>
+                  <div className="text-muted" style={{ fontSize: '0.7rem' }}>
+                    {generateRandomDay()}d ago
+                  </div>
+                </div>
               </div>
               <span>{post.user.title}</span>
               <Card.Text>{post.text}</Card.Text>
@@ -80,7 +93,7 @@ const HomePagePosts = () => {
               )}
               {/* //    <img className="" width={250} alt="img post" src={post.image} /> */}
             </Card.Body>
-            <div className="d-flex justify-content-evenly">
+            <div className="d-flex justify-content-evenly mt-1">
               <Button
                 className={`d-flex align-items-center button-homepage${
                   liked ? ' text-primary' : ''
@@ -96,24 +109,21 @@ const HomePagePosts = () => {
               </Button>
               <Button
                 className="d-flex align-items-center button-homepage"
-                onClick={() => {
-                  toggleCommentSection(i);
-                  setSmShow(true);
-                }}
+                onClick={toggleCommentSection}
               >
                 <i className="bi bi-chat-left-dots me-2"></i>
                 <span>Comment</span>
               </Button>
-              {/* {commentingStates[i] && (
-        // <div className="comment-section d-flex align-items-center">
-        //  <textarea placeholder="Inserisci il tuo commento"></textarea>
-        //  <div>
-        // <Button className="ms-1" variant="outline-info">
-        //  Invia
-        // </Button>
-        //  </div>
-        // </div>
-       )} */}
+              {isCommenting && (
+                <div className="comment-section d-flex align-items-center">
+                  <textarea placeholder="Inserisci il tuo commento"></textarea>
+                  <div>
+                    <Button className="ms-1" variant="outline-info">
+                      Invia
+                    </Button>
+                  </div>
+                </div>
+              )}
               <Button className="button-homepage">
                 <i className="bi bi-repeat me-2"></i>Repost
               </Button>
@@ -124,33 +134,6 @@ const HomePagePosts = () => {
           </Card>
         </Col>
       ))}
-      {/* MODALE COMMENTI */}
-
-      <Modal
-        size="sm"
-        show={smShow}
-        onHide={() => setSmShow(false)}
-        aria-labelledby="contained-modal-title-vcenter"
-        centered
-      >
-        <Modal.Header closeButton>
-          <Modal.Title id="contained-modal-title-vcenter">Comment</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Form>
-            <Form.Group
-              className="mb-3"
-              controlId="exampleForm.ControlTextarea1"
-            >
-              <Form.Control
-                as="textarea"
-                rows={3}
-                placeholder="Write your comment here"
-              />
-            </Form.Group>
-          </Form>
-        </Modal.Body>
-      </Modal>
     </>
   );
 };
